cba60fd61cadb66c02ad7cdb14adb454a656098a
Irena Vakulenko <ivvvv@ukr.net>
2021-03-04 21:16:59 +0200

    Домашка 9.3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,65 @@
-/*Домашка 9. 4*) Подсчитать количество треугольников, у которых стороны заданы целыми числами,
-и площадь которых не больше 100
-
- 4.1*) Сделать задание так чтобы не было повторяющихся треугольников, например (3,4,5) и (5,3,4) - повторяющиеся. */
+/*Домашка 9.*3*)Написать метод который ищет при помощи бинарного поиска
+        есть ли число в отсортированном массиве*/
 
 package com.company;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        double square;
-        int count = 0;
-        for (int i = 1; i < 1000; i++) {
-            for (int j = 1; j < 1000; j++) {
-                for (int k = 1; k < 1000; k++) {
-                    square = formulaGerona(i, j, k);
-                    if (square <= 100 & square > 0) {
-                        count++;
-                    }
-                }
-            }
+        int[] arr = createRandomArray();
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число, которое надо найти в массиве:");
+        int number = scanner.nextInt();
+        if (isInTheArray(arr, number, 0, arr.length - 1)) {
+            System.out.println("Число находится в массиве");
+        } else {
+            System.out.println("Числа нет в массиве");
         }
-        System.out.println(count + " треугольников,");
-        System.out.println("из них неповтояющихся: " + count / 3);
-        System.out.println("или как предполагалось сделать?");
     }
 
-    private static double formulaGerona(int i, int j, int k) {
-        double halfPerimetr = (i + j + k) / 2.0;
-        try {
-            double square = Math.sqrt(halfPerimetr * (halfPerimetr - i) * (halfPerimetr - j) * (halfPerimetr - k));
-            return square;
-        } catch (Exception e) {
-            return 999;
+    private static boolean isInTheArray(int[] arr, int number, int indexBegin, int indexEnd) {
+        int middle = indexBegin + (indexEnd - indexBegin) / 2;
+        if (number == arr[middle]) {
+            return true;
+        } else if (middle == 0) {
+            if (arr.length > 1) {
+                if (arr[1] == number) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            return false;
+        } else if (middle == arr.length - 2) {
+            if (number == arr[arr.length - 1] || number == arr[0]) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (number > arr[middle]) {
+                indexBegin = middle;
+            } else {
+                indexEnd = middle;
+            }
+            return isInTheArray(arr, number, indexBegin, indexEnd);
         }
+    }
+
+    private static int[] createRandomArray() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите длину массива");
+        int length = scanner.nextInt();
+        int[] arr = new int[length];
+        Random random = new Random();
+        for (int i = -0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100);
+        }
+        return arr;
     }
 }
