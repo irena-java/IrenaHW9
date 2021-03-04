61b5d98cac6a681c40f33406a044fa4999f37899
Irena Vakulenko <ivvvv@ukr.net>
2021-03-04 21:22:16 +0200

    Домашка 9.3 первый вариант, кот.не получился, т.к.get-catch не захотел отлавливать бесконечную рекурсию, не  заходил в catch, а выбрасывал exception

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
-/*Домашка 9.*3*)Написать метод который ищет при помощи бинарного поиска
-        есть ли число в отсортированном массиве*/
+/*Домашка 9.3 вариант 1 не получился, вопрос по отлавливанию get-catch бесконечной
+рекурсии - почему не отлавливает, дальше сделала отдельным коммитом переделанный вариант,
+ без использования get-catch
+Написать метод который ищет при помощи бинарного поиска есть ли число в
+отсортированном массиве*/
 
 package com.company;
 
@@ -9,38 +12,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = createRandomArray();
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число, которое надо найти в массиве:");
         int number = scanner.nextInt();
-        if (isInTheArray(arr, number, 0, arr.length - 1)) {
-            System.out.println("Число находится в массиве");
-        } else {
-            System.out.println("Числа нет в массиве");
+        int[] arr = createRandomArray();
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        try {
+            System.out.println(isInTheArray(arr, number, 0, arr.length - 1));
+        } catch (Exception e) {
+            System.out.println("Этого числа нет в массиве");
         }
     }
 
     private static boolean isInTheArray(int[] arr, int number, int indexBegin, int indexEnd) {
+        //try {
         int middle = indexBegin + (indexEnd - indexBegin) / 2;
         if (number == arr[middle]) {
             return true;
-        } else if (middle == 0) {
-            if (arr.length > 1) {
-                if (arr[1] == number) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-            return false;
-        } else if (middle == arr.length - 2) {
-            if (number == arr[arr.length - 1] || number == arr[0]) {
-                return true;
-            } else {
-                return false;
-            }
         } else {
             if (number > arr[middle]) {
                 indexBegin = middle;
@@ -49,7 +39,12 @@ public class Main {
             }
             return isInTheArray(arr, number, indexBegin, indexEnd);
         }
+        //} catch (Exception e) {
+        //  System.out.println("gdfdgdfg");
+//            return false;
+        //      }
     }
+
 
     private static int[] createRandomArray() {
         Scanner scanner = new Scanner(System.in);
@@ -58,8 +53,18 @@ public class Main {
         int[] arr = new int[length];
         Random random = new Random();
         for (int i = -0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100);
+            arr[i] = random.nextInt();
         }
+        System.out.println(Arrays.toString(arr));
+
+        arr[0] = 50;
+        arr[1] = 60;
+        arr[2] = 75;
+        arr[3] = 80;
+        arr[4] = 90;
+        arr[5] = 100;
+        arr[6] = 150;
+
         return arr;
     }
 }
